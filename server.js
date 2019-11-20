1ce13571a6bb7075d645dcec6ca2f3dcbaa39261
tyntin091 <tyntin091@gmail.com>
2019-11-21 05:15:55 +0700

    fix bug

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const chatController = require('./controllers/ConversationController')
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'build')));
-    app.get('/', function (req, res) {
+    app.get('/*', function (req, res) {
         res.sendFile(path.join(__dirname, 'build', 'index.html'));
     });
 } else {
